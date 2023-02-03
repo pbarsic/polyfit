25413df70650187bcfbcb4f65f1028ed77c32e65
Paul Barsic <paul.barsic@irisonboard.com>
2023-02-02 18:17:48 -0800

    performance measurement

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
 
 #include "polyfit.hpp"
 
 int main() {
+
   // application to demonstrate the polynomial fit
-  int npts = 10;
+  // and also provide the amount of time to compute
+  int npts = 100;
+  int num_iterations = 1000;
+  cv::TickMeter tm;
   std::vector<double> input_coeffs{73, 14, -2.8};
   std::vector<double> xpoints;
   double xmin = -4.5, xdelta = 2.1;
@@ -22,7 +27,17 @@ int main() {
   make_poly(xpoints, input_coeffs, std, &xypoints);
   make_poly(xpoints, input_coeffs, 0, &clean_points);
 
-  polyfit(xypoints, &fit_coeffs);
+  for (int ii = 0; ii < num_iterations; ii++) {
+    tm.start();
+    polyfit(xypoints, &fit_coeffs);
+    tm.stop();
+  }
+  double avg_time_milli =
+      tm.getTimeMilli() / static_cast<double>(num_iterations);
+  std::cout << "Average time per " << npts
+            << "-point polynomial fit in milliseconds: " << avg_time_milli
+            << std::endl;
+
   make_poly(xpoints, fit_coeffs, std, &model_points);
 
   std::cout << "x, y(x) + noise, y(x), fit(x)" << std::endl;
